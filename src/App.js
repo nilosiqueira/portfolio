47af818c9aecb586d1ce79f9b0799717825ca0d9
Nilo Siqueira <nilosiqueiratj@gmail.com>
2021-11-28 17:55:47 -0300

    create menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="wrapper">
+        <div class="header">
+            <h1>Olá</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </div>
+    <main>
+        <section class="module parallax parallax-1">
+            <h1>Nilo Siqueira</h1>
+        </section>
+        <section class="module content">
+            <div class="container">
+                <h2>Qualquer texto aqui...</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Est amet nulla praesentium, consequatur nesciunt inventore 
+                    tempore architecto, beatae natus ducimus fuga quod autem 
+                    tempora voluptas hic provident, ad nisi aspernatur.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Est amet nulla praesentium, consequatur nesciunt inventore 
+                    tempore architecto, beatae natus ducimus fuga quod autem 
+                    tempora voluptas hic provident, ad nisi aspernatur.
+                </p>
+            </div>
+        </section>
+
+        <section class="module parallax parallax-2">
+            <h1>Algum testo aqui...</h1>
+        </section>
+        <section class="module content">
+            <div class="container">
+                <h2>Qualquer texto aqui...</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Est amet nulla praesentium, consequatur nesciunt inventore 
+                    tempore architecto, beatae natus ducimus fuga quod autem 
+                    tempora voluptas hic provident, ad nisi aspernatur.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Est amet nulla praesentium, consequatur nesciunt inventore 
+                    tempore architecto, beatae natus ducimus fuga quod autem 
+                    tempora voluptas hic provident, ad nisi aspernatur.
+                </p>
+            </div>
+        </section>
+        <section class="module parallax parallax-3">
+            <h1>Outro texto aqui</h1>
+        </section>
+        <section class="module content">
+            <div class="container">
+                <h2>Qualquer texto aqui...</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Est amet nulla praesentium, consequatur nesciunt inventore 
+                    tempore architecto, beatae natus ducimus fuga quod autem 
+                    tempora voluptas hic provident, ad nisi aspernatur.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Est amet nulla praesentium, consequatur nesciunt inventore 
+                    tempore architecto, beatae natus ducimus fuga quod autem 
+                    tempora voluptas hic provident, ad nisi aspernatur.
+                </p>
+            </div>
+        </section>
+    </main>
+</div>
   );
 }
 
